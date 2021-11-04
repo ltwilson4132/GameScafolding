b340840b95848945e23ea9168dd0de55f7a2b8dc
ltwilson4132 <78631506+ltwilson4132@users.noreply.github.com>
2021-11-03 22:46:30 -0400

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -44,4 +44,10 @@ public class Dice
         //Generates random number between 1 and 20.
         return 0;
     }
+    
+    public int generateRandomNum(int limit)
+    {
+        //This method will be used to generate a random number between 0 and the limit or 1 and the limit.
+        return 0;
+    }
 }//end class Dice
