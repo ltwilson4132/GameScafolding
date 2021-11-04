05b72b76741c518b8a6fbfdd0135e00b0c417c56
ltwilson4132 <78631506+ltwilson4132@users.noreply.github.com>
2021-11-03 22:06:03 -0400

    Update BattleSystem.java

--- BattleSystem.java
+++ BattleSystem.java
@@ -1,26 +1,30 @@
 public class BattleSystem //extends Dice Should not extend Dice. BattleSystem uses Dice.
 {
-    private String battleType;
+    //private String battleType;
 
-    public String startBattle(Entity entity1, Entity entity2)
+    public static String startBattle(Entity entity1, Entity entity2)
     {
         //Method that starts a battle between 2 entities
+        //Check to see if either entity is dead. 
+        //While neither entity is dead do battle.
+        //Outputs string that says the results of the battle.
         return "";
     }
 
     public void Turn()
     {
-        //Give player options for their turn. Heal or Attack.
+        //Give player options for their turn. Use item or attack enemy. To be used by the character/player
     }
 
-    public void Heal(Entity player)
+    public void UseItem(Entity player)
     {
-        //Will heal the player and/or use item.
+        //Will let the player see what items they have and then give them the chance to use an item.
+        //Apply effects of item and then remove item from inventory.
     }
 
     public void Attack(Entity attacker, Entity defender)
     {
-        //Roll twenty dice to decide attack power and then do damage to opposing entity.
+        //Roll twenty dice to decide attack power, take attack power minus defenders defense and take that amount of health from the defender.
     }
 
     /*
