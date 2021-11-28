a9a3851ff1c1cb4748ee40fe183de2bc08cc978e
ConnorW17 <93808036+ConnorW17@users.noreply.github.com>
2021-11-28 01:42:55 -0500

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class Location
+{
+    private String locationName, locationDescription;
+    final int size; //How many enemies it can hold
+    private ArrayList<Monster> monsters = new ArrayList<Monster>();
+
+    public Location(String locationName, String locationDescription, int size)
+    {
+        this.locationName = locationName;
+        this.locationDescription = locationDescription;
+        this.size = size; //Equals max size that location can hold
+        //Generates random number of Monsters between 0 and the max size of the location and adds them to the ArrayList
+    }
+
+    public String getLocationName()
+    {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName)
+    {
+        this.locationName = locationName;
+    }
+
+    public String getLocationDescription()
+    {
+        return locationDescription;
+    }
+
+    public void setLocationDescription(String locationDescription)
+    {
+        this.locationDescription = locationDescription;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public String getMonsters()
+    {
+        //Outputs String representing all monster at location.
+        return "";
+    }
+
+    public void AddMonster(Monster monster)
+    {
+        //Adds a new monster to the location's ArrayList
+    }
+
+    public void removeMonster(Monster monster)
+    {
+        //Removes a monster from the ArrayList when one is killed
+    }
+}
---
+++ Monster.java
@@ -0,0 +1,55 @@
+/*
+ * -------------------------------------------------------------------------------
+ * File name: Monster.java
+ * Project name: Monster
+ * -------------------------------------------------------------------------------
+ * Author's name and email: Logan Wilson wilsonlt@etsu.edu
+ * Course-Section: CSCI-1250-900
+ * Creation Date: Oct 27, 2021
+ * Last modified: Logan Wilson wilsonlt@etsu.edu Oct 27, 2021
+ * -------------------------------------------------------------------------------
+ */
+
+/*
+ * Class Name: Monster<br>
+ * Class Purpose:  <br>
+ *
+ * <hr>
+ * Date created: Oct 27, 2021<br>
+ * Last modified: Oct 27, 2021
+ * @author Logan Wilson
+ */
+
+import java.util.ArrayList;
+
+public class Monster extends Entity implements Damage
+{
+    private MonsterType enemyType;
+
+    public Monster(MonsterType enemyType, int hp, int def)
+    {
+        super(hp, def);
+        this.enemyType = enemyType;
+    }
+
+    public String MonsterSpawn(Location location)
+    {
+        //This method will spawn monsters in a location upon location's creation.
+        return "";
+    }
+
+    public MonsterType getEnemyType()
+    {
+        return enemyType;
+    }
+
+    public void setEnemyType(MonsterType enemyType)
+    {
+        this.enemyType = enemyType;
+    }
+
+    public int Attack()
+    {
+        return Dice.rollDiceTwenty();
+    }
+}//end class Monster
