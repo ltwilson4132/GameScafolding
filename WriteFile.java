dbd6b83dffe5e6eab301c87cba858392d4308526
Byars98 <71306177+Byars98@users.noreply.github.com>
2021-11-29 18:16:18 -0500

    Add files via upload

--- WriteFile.java
+++ WriteFile.java
@@ -27,13 +27,14 @@ import java.util.HashMap;
 
 public class WriteFile
 {
-    public static void SaveGame(Character player, ArrayList<Location> locations)
+    public static void SavePlayer(Character player)
     {
         //Writes the info of the player's character and the map to a text file to save the game.
         try {
-            FileWriter savePlayer = new FileWriter("saveFile.txt");
+            FileWriter savePlayer = new FileWriter("SavedPlayer.txt");
             savePlayer.write(String.valueOf(player));
-            savePlayer.write(String.valueOf(locations));
+            savePlayer.write(String.valueOf(player.getCurrentLocation()));
+            savePlayer.write(String.valueOf(player.cInventory));
             savePlayer.close();
             System.out.println("Game Saved");
         } catch (IOException e) {
@@ -41,4 +42,20 @@ public class WriteFile
             e.printStackTrace();
         }
     }
-} //end class WriteFile
+
+    public static void SaveMap(ArrayList<Location> locations)
+    {
+        {
+            //Writes the info of the player's character and the map to a text file to save the game.
+            try {
+                FileWriter savePlayer = new FileWriter("SavedMap.txt");
+                savePlayer.write(String.valueOf(locations));
+                savePlayer.close();
+                System.out.println("Game Saved");
+            } catch (IOException e) {
+                System.out.println("Could Not Save Game");
+                e.printStackTrace();
+            }
+        }
+    }
+}
