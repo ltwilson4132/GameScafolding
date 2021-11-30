1047618068e71894641e65e879f02e9464aa2f9c
scrainss <61420120+scrainss@users.noreply.github.com>
2021-11-29 19:37:11 -0500

    Add files via upload

--- ReadFile.java
+++ ReadFile.java
@@ -31,7 +31,7 @@ public class ReadFile
      *
      * <hr>
      * Date created: 11/11/2021 <br>
-     * Date last modified: 11/14/2021 <br>
+     * Date last modified: 11/29/2021 <br>
      * <hr>
      *
      * @param player
@@ -41,24 +41,24 @@ public class ReadFile
     {
         Character savedPlayer = player;
         HashMap<String, Location> savedMap = map;
-       try
-       {
-           File savedFile = new File("saveFile.txt"); //opens the file to be read from
-           Scanner saveReader = new Scanner(savedFile); // creates a Scanner to read from the file
+        try
+        {
+            File savedFile = new File("saveFile.txt"); //opens the file to be read from
+            Scanner saveReader = new Scanner(savedFile); // creates a Scanner to read from the file
 
-           while (saveReader.hasNextLine()) //while there is another line, the reader will read the data and then print it
-           {
-               String saveData = saveReader.nextLine();
-               System.out.println(saveData);
-           }
+            while (saveReader.hasNextLine()) //while there is another line, the reader will read the data and then print it
+            {
+                String saveData = saveReader.nextLine();
+                System.out.println(saveData);
+            }
 
-           saveReader.close(); // close the save file
-           System.out.println("Save loaded!");
-       }
-       catch (IOException notFound) // handles exception when save file is not found
-       {
-           System.out.println("Save data not found!");
-       }
+            saveReader.close(); // close the save file
+            System.out.println("Save loaded!");
+        }
+        catch (IOException notFound) // handles exception when save file is not found
+        {
+            System.out.println("Save data not found!");
+        }
     }
 
     /**
@@ -68,7 +68,7 @@ public class ReadFile
      *
      * <hr>
      * Date created: 11/22/21 <br>
-     * Date last modified: 11/22/21 <br>
+     * Date last modified: 11/29/21 <br>
      * <hr>
      *
      * @param path
@@ -82,18 +82,66 @@ public class ReadFile
 
         try(BufferedReader myLocationReader = new BufferedReader(new FileReader(path)))
         {
-                while((locationEntry = myLocationReader.readLine()) != null)
-                {
-                    String[] locationInfo = locationEntry.split(",");
+            while((locationEntry = myLocationReader.readLine()) != null)
+            {
+                String[] locationInfo = locationEntry.split(",");
 
-                    Location location = new Location(locationInfo[0], locationInfo[1], Integer.parseInt(locationInfo[2]));
+                Location location = new Location(locationInfo[0], locationInfo[1], Integer.parseInt(locationInfo[2]));
 
-                    locations.add(location);
-                }//end while
+                locations.add(location);
+            }//end while
         }//end try
         catch(IOException exc)
         {
             System.out.println("File not found!");
         }
     }
+
+    /**
+     * Method Name: loadPlayer <br>
+     * Method Purpose: reads file ...<br>
+     *
+     * <hr>
+     * Date created: 11/29/21 <br>
+     * Date last modified: 11/29/21 <br>
+     * <hr>
+     *
+     * @param locations
+     *
+     */
+    public static Character loadPlayer(ArrayList<Location> locations) throws FileNotFoundException {
+        String entry;
+        try (BufferedReader playerReader = new BufferedReader(new FileReader("SavedPlayer.txt")))
+        {
+            entry = playerReader.readLine();
+            String[] playerInfo = entry.split(",");
+            String name = playerInfo[0];
+            CharacterType type;
+            if(playerInfo[1].equals("Archer"))
+            {
+                type = CharacterType.ARCHER;
+            } else if(playerInfo[1].equals("Knight")) {
+                type = CharacterType.KNIGHT;
+            } else {
+                type = CharacterType.WIZARD;
+            }
+            int health = Integer.parseInt(playerInfo[2]);
+            int defense = Integer.parseInt(playerInfo[3]);
+            Character player = new Character(name, type, health, defense);
+            for (Location location: locations)
+            {
+                if(location.getLocationName().equals(playerInfo[4])){
+                    player.setCurrentLocation(location);
+                }
+            }
+            player.setAttackBoost(Integer.parseInt(playerInfo[5]));
+            player.setDefenseBoost(Integer.parseInt(playerInfo[6]));
+            player.
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
